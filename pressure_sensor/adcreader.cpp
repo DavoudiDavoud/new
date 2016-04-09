d816b0cc57e0b74638c18a8972e347da715f62a7
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 15:31:26 +0100

    Update adcreader.cpp

--- pressure_sensor/adcreader.cpp
+++ pressure_sensor/adcreader.cpp
@@ -101,84 +101,83 @@ ADCreader::ADCreader()
   writeReg(fd,0x40);
 }
 
-void ADCreader::writeReset(int fd)
+static void writeReset(int fd)
 {
-        int ret;
-        uint8_t tx1[5] = {0xff,0xff,0xff,0xff,0xff};
-        uint8_t rx1[5] = {0};
-        struct spi_ioc_transfer tr;
-	tr.tx_buf = (unsigned long)tx1;
-	tr.rx_buf = (unsigned long)rx1;
-	tr.len = ARRAY_SIZE(tx1);
-	tr.delay_usecs = delay;
-	tr.speed_hz = speed;
-	tr.bits_per_word = bits;
+  int ret;
+  uint8_t tx1[5] = {0xff,0xff,0xff,0xff,0xff};
+  uint8_t rx1[5] = {0};
+  struct spi_ioc_transfer tr;
 
-        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-        if (ret < 1)
-                pabort("can't send spi message");
+  memset(&tr,0,sizeof(struct spi_ioc_transfer));
+  tr.tx_buf = (unsigned long)tx1;
+  tr.rx_buf = (unsigned long)rx1;
+  tr.len = sizeof(tx1);
+
+  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+  if (ret < 1) {
+    printf("\nerr=%d when trying to reset. \n",ret);
+    pabort("Can't send spi message");
+  }
 }
-
-
-void ADCreader::writeReg(int fd, uint8_t v)
+static void writeReg(int fd, uint8_t v)
 {
   int ret;
   uint8_t tx1[1];
   tx1[0] = v;
   uint8_t rx1[1] = {0};
   struct spi_ioc_transfer tr;
+
+  memset(&tr,0,sizeof(struct spi_ioc_transfer));
   tr.tx_buf = (unsigned long)tx1;
   tr.rx_buf = (unsigned long)rx1;
-  tr.len = ARRAY_SIZE(tx1);
-  tr.delay_usecs = delay;
-  tr.speed_hz = speed;
-  tr.bits_per_word = bits;
-	
-        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-        if (ret < 1)
-                pabort("can't send spi message");
+  tr.len = sizeof(tx1);
 
+  ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+  if (ret < 1)
+    pabort("can't send spi message");
 }
 
-uint8_t ADCreader::readReg(int fd)
+
+static uint8_t readReg(int fd)
 {
-        int ret;
-        uint8_t tx1[1];
-        tx1[0] = 0;
-        uint8_t rx1[1] = {0};
-        struct spi_ioc_transfer tr;
+	int ret;
+	uint8_t tx1[1];
+	tx1[0] = 0;
+	uint8_t rx1[1] = {0};
+	struct spi_ioc_transfer tr;
+
+	memset(&tr,0,sizeof(struct spi_ioc_transfer));
 	tr.tx_buf = (unsigned long)tx1;
 	tr.rx_buf = (unsigned long)rx1;
-	tr.len = ARRAY_SIZE(tx1);
-	tr.delay_usecs = delay;
-	tr.speed_hz = speed;
-	tr.bits_per_word = 8;
+	tr.len = sizeof(tx1);
 
-        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-        if (ret < 1)
-          pabort("can't send spi message");
-          
-        return rx1[0];
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+	  pabort("can't send spi message");
+	  
+	return rx1[0];
 }
 
-int ADCreader::readData(int fd)
+static int readData(int fd)
 {
-        int ret;
-        uint8_t tx1[2] = {0,0};
-        uint8_t rx1[2] = {0,0};
-        struct spi_ioc_transfer tr;
+	int ret;
+	uint8_t tx1[2] = {0,0};
+	uint8_t rx1[2] = {0,0};
+	struct spi_ioc_transfer tr;
+
+	memset(&tr,0,sizeof(struct spi_ioc_transfer));
 	tr.tx_buf = (unsigned long)tx1;
 	tr.rx_buf = (unsigned long)rx1;
-	tr.len = ARRAY_SIZE(tx1);
-	tr.delay_usecs = delay;
-	tr.speed_hz = speed;
-	tr.bits_per_word = 8;
+	tr.len = sizeof(tx1);
 
-        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
-        if (ret < 1)
-          pabort("can't send spi message");
-          
-        return (rx1[0]<<8)|(rx1[1]);
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 1)
+          {
+	  printf("\n can't send spi message, ret = %d\n",ret);
+          exit(1);
+          }
+	  
+	return (rx1[0]<<8)|(rx1[1]);
 }
 
 
